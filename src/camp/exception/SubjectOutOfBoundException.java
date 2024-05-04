ede76aaa6bac9445abca3eb0a35e94ab4167434f
JKhan0408 <166383040+JKhan0408@users.noreply.github.com>
2024-05-05 01:43:00 +0900

    Delete src/camp/exception directory

--- src/camp/exception/SubjectOutOfBoundException.java
+++
@@ -1,8 +0,0 @@
-package camp.exception;
-
-// 존재하지 않는 과목을 입력했을 때 발생하는 예외
-public class SubjectOutOfBoundException extends Exception{
-    public SubjectOutOfBoundException() {
-        super("없는 과목입니다!\n");
-    }
-}
