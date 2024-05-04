a8574b82e20c2a7d901aec50a483653c785b1b5c
JKhan0408 <workhjk804@gmail.com>
2024-05-05 01:19:06 +0900

    수강생 등록 - InitializeData의 sequence 함수를 StudentDAO로 이동해서 고유번호 부여

--- src/camp/function/StudentDAO.java
+++ src/camp/function/StudentDAO.java
@@ -1,9 +1,11 @@
 package camp.function;
 
+import camp.CampManagementApplication;
 import camp.exception.NotEnoughSubjectsException;
 import camp.exception.NotStatusException;
 import camp.exception.SubjectOutOfBoundException;
 import camp.model.Student;
+import camp.model.Subject;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -13,21 +15,23 @@ public class StudentDAO {
     // 스캐너
     private static Scanner sc = new Scanner(System.in);
 
-    // 고유번호 부여
     InitializeData initializeData = new InitializeData();
+    // 고유 번호
+    private int studentIndex = 0;
+    private static final String INDEX_TYPE_STUDENT = "ST";
 
     // 데이터 저장 리스트
     private List<Student> studentStore = new LinkedList<>();
 
     // 수강생 등록
     public void createStudent() {
-        String studentID = initializeData.sequence(InitializeData.INDEX_TYPE_STUDENT);
+        String studentID = sequence();
         String studentName = " ";
         String studentStatus = " ";
         LinkedList<String> statusTypes = new LinkedList<>(List.of("green", "yellow", "red"));
         LinkedList<String> studentSubjects = new LinkedList<>();
         LinkedList<String> mandatorySubjects = new LinkedList<>(List.of("Java", "객체지향", "Spring", "JPA", "MySQL")); // 최소 3개 이상
-        LinkedList<String> choiceSubjects = new LinkedList<>(List.of("디자인 패턴", "Spring Security", "Redis", "MongoDB")); // 최소 3개 이상
+        LinkedList<String> choiceSubjects = new LinkedList<>(List.of("디자인 패턴", "Spring Security", "Redis", "MongoDB")); // 최소 2개 이상
         int countMandatory = 0;
         int countChoice = 0;
         boolean printStatus = true;
@@ -131,4 +135,13 @@ public class StudentDAO {
         // 기능 구현
         System.out.println("\n수강생 목록 조회 성공!");
     }
+
+    // 고유 번호 증가
+    public String sequence() {
+        studentIndex++;
+        return INDEX_TYPE_STUDENT + studentIndex;
+    }
 }
+
+
+
