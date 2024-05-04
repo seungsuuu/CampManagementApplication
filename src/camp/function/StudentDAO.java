e94f3ff778cc601e45ae720fd4c53d7b87f82dc2
JKhan0408 <166383040+JKhan0408@users.noreply.github.com>
2024-05-05 01:48:39 +0900

    String[9] Student 배열 -> 연결리스트로 변경

--- src/camp/function/StudentDAO.java
+++ src/camp/function/StudentDAO.java
@@ -2,6 +2,8 @@ package camp.function;
 
 import camp.model.Student;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class StudentDAO {
@@ -13,14 +15,17 @@ public class StudentDAO {
 
     // 수강생 등록
     public void createStudent() {
-        System.out.println("\n수강생을 등록합니다...");
-        System.out.print("수강생 이름 입력: ");
-        String studentName = sc.next();
-        String studentStatus=" ";
-        String[] studentSubjects = new String[9];
+        String studentID = initializeData.sequence(InitializeData.INDEX_TYPE_STUDENT);
+        String studentName = " ";
+        String studentStatus = " ";
+        LinkedList<String> studentSubjects = new LinkedList<>();
+        String[] mandatorySubjects = new String[]{"Java", "객체지향", "Spring", "JPA", "MySQL"}; // 최소 3개 이상
+        String[] choiceSubjects = new String[]{"디자인 패턴", "Spring Security", "Redis", "MongoDB"}; // 최소 2개 이상
+        int count = 0;
+
         // 기능 구현 (필수 과목, 선택 과목)
 
-        Student student = new Student(initializeData.sequence(InitializeData.INDEX_TYPE_STUDENT), studentName, studentStatus, studentSubjects); // 수강생 인스턴스 생성 예시 코드
+        Student student = new Student(studentID, studentName, studentStatus, studentSubjects); // 수강생 인스턴스 생성 예시 코드
         // 기능 구현
         System.out.println("수강생 등록 성공!\n");
     }
