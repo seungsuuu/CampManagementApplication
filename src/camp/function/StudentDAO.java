9ed468a0d0635f65e6453c7ca94418c013c008f6
이세비 <lsy205105@naver.com>
2024-05-06 20:55:27 +0900

    수강생 목록 조회 추가

--- src/camp/function/StudentDAO.java
+++ src/camp/function/StudentDAO.java
@@ -2,10 +2,9 @@ package camp.function;
 
 import camp.model.Student;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Stream;
+
 
 public class StudentDAO {
     private static Scanner sc = new Scanner(System.in);
@@ -47,7 +46,7 @@ public class StudentDAO {
         ).toList());
     }
 
-      // 수강생 등록
+    // 수강생 등록
     public void createStudent() {
         String studentID = initializeData.sequence(InitializeData.INDEX_TYPE_STUDENT);
         String studentName = " ";
@@ -57,18 +56,40 @@ public class StudentDAO {
         String[] choiceSubjects = new String[]{"디자인 패턴", "Spring Security", "Redis", "MongoDB"}; // 최소 2개 이상
         int count = 0;
 
-        // 기능 구현 (필수 과목, 선택 과목)
 
         Student student = new Student(studentID, studentName, studentStatus, studentSubjects); // 수강생 인스턴스 생성 예시 코드
         // 기능 구현
         System.out.println("수강생 등록 성공!\n");
     }
 
+
     // 수강생 목록 조회
     public void inquireStudent() {
+        List<Student> students = getStudentStore();
+
         System.out.println("\n수강생 목록을 조회합니다...");
-        // 기능 구현
-        System.out.println("\n수강생 목록 조회 성공!");
+        System.out.print("조회할 학생의 이름을 입력하세요 : ");
+        String name = sc.next();
+        System.out.print("조회할 학생의 고유번호을 입력하세요 : ");
+        String index = sc.next();
+
+        Optional<Student> optionalStudentName = students.stream().filter(s-> s.getStudentName().equals(name)).findFirst();
+        Optional<Student> optionalStudentIndex = students.stream().filter(s -> s.getStudentId().equals(index)).findFirst();
+
+        Student student;
+
+        if(optionalStudentIndex.isPresent() && optionalStudentName.isPresent()){
+            student = optionalStudentIndex.get();
+            System.out.println("=== 조회내용 ===");
+            System.out.println("이름: "+student.getStudentName());
+            System.out.println("고유번호 : "+student.getStudentId());
+            System.out.println("상태 : "+student.getStudentStatus());
+            System.out.println("과목 : "+student.getStudentSubjects());
+            System.out.println("\n수강생 목록 조회 성공!");
+        }else{
+            System.out.println("다시한번 확인해주세요.");
+        }
+
     }
 
     // 고유 번호 증가
