b2dbccf35a8356526a1488907730b262417c5960
seungsuuu <tmdtn013@naver.com>
2024-05-06 20:24:43 +0900

    시험 회차별 등급 조회 성공

--- src/camp/function/ScoreDAO.java
+++ src/camp/function/ScoreDAO.java
@@ -1,10 +1,8 @@
 package camp.function;
 
 import camp.model.Score;
-import camp.model.Student;
 import camp.model.Subject;
 
-import javax.swing.plaf.synth.SynthColorChooserUI;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
@@ -39,7 +37,7 @@ public class ScoreDAO {
                         'F'
                 ),
                 new Score(
-                        studentDAO.getStudentStore().get(2).getStudentId(),
+                        studentDAO.getStudentStore().get(0).getStudentId(),
                         subjectDAO.getSubjectStore().get(2).getSubjectId(),
                         1,
                         30,
@@ -50,17 +48,24 @@ public class ScoreDAO {
 
     // 수강생 고유번호 입력받음
     public String enterStudentId() {
-        System.out.print("\n관리할 수강생의 번호를 입력하시오...");
+        System.out.print("\n수강생의 번호를 입력하시오...");
         Scanner sc = new Scanner(System.in);
         return sc.next();
     }
 
     // 과목 이름 입력받기
     private String enterSubjectName() {
-        System.out.print("\n관리할 과목의 이름을 입력하시오...");
+        System.out.print("\n과목의 이름을 입력하시오...");
         Scanner sc = new Scanner(System.in);
         // 공백이 포함된 문장도 있으므로 nextLine을 사용한다
         return sc.nextLine();
+    }
+
+    // 과목 이름 입력받기
+    private int enterRound() {
+        System.out.print("\n과목의 시험 회차를 입력하시오...");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 
     // 수강생의 과목별 시험 회차 및 점수 등록
@@ -82,55 +87,65 @@ public class ScoreDAO {
 
     // 수강생의 특정 과목 회차별 등급 조회
     public void inquireRoundRankBySubject() {
-        LinkedList<String> studentSubjects = new LinkedList<>();
+        List<Score> inquireScore = new LinkedList<>();
+        Score resultScore = new Score();
 
-        boolean studentIdCheck = false;
-        boolean subjectCheck = false;
+        String studentId = enterStudentId(); // 조회할 수강생 고유 번호
+        String subjectName = enterSubjectName(); // 조회할 수강 과목 이름
+        int round = enterRound(); // 죄회할 성적 회차
 
-        // 기능 구현 (조회할 수강생의 조회할 특정 과목)
-        String studentId = enterStudentId(); // 관리할 수강생 고유 번호
-        for(Student st : studentDAO.getStudentStore()) {
-            if (Objects.equals(st.getStudentId(), studentId)){
-                System.out.println("수강생 번호 일치");
-                studentIdCheck = true;
-                studentSubjects = st.getStudentSubjects();
+        String subjectId = "";
+        for (Subject subject : subjectDAO.getSubjectStore()){
+            if(Objects.equals(subjectName, subject.getSubjectName())) {
+                subjectId = subject.getSubjectId();
                 break;
             } else {
-                System.out.println("입력된 수강생 번호와 일치하는 수강생이 없습니다.");
+                System.out.println("수강 과목 이름을 잘못 입력하였습니다.");
                 return;
             }
         }
 
-        String subjectName = enterSubjectName(); // 관리할 수강 과목
-        for (String sj : studentSubjects) {
-            if(Objects.equals(sj, subjectName)) {
-                System.out.println("수강생 과목 일치");
-                subjectCheck = true;
-                break;
-            } else {
-                System.out.println("입력된 수강생 과목과 일치하는 수강생의 수강 과목이 없습니다.");
-                return;
+        int countStudentId = 0;
+        for(Score score : scoreStore) {
+            if (Objects.equals(score.getStudentId(), studentId)){
+                inquireScore.add(score);
+                countStudentId++;
             }
         }
+        if (countStudentId == 0) {
+            System.out.println("입력된 수강생 번호를 잘못 입력하였거나, 입력된 수강생 번호와 일치하는 수강생의 점수가 없습니다.");
+            return;
+        }
 
-        String subjectID;
-        for (Subject sjs : subjectDAO.getSubjectStore()){
-            if(Objects.equals(subjectName, sjs.getSubjectName())) {
-                subjectID = sjs.getSubjectId();
-                break;
+        int countSubjectId = 0;
+        for(Score score: inquireScore) {
+            if (!Objects.equals(score.getSubjectId(), subjectId)){
+                inquireScore.remove(score);
             } else {
-                System.out.println("수강 과목 이름에서 ID로 변경 실패!");
-                return;
+                countSubjectId++;
             }
         }
-
-        System.out.println("회차별 등급을 조회합니다...");
-        // 기능 구현
-        if (studentIdCheck && subjectCheck ) {
-
+        if (countSubjectId == 0) {
+            System.out.println("입력된 수강 과목과 일치하는 과목의 점수가 없습니다.");
+            return;
         }
 
+        int countRound = 0;
+        for (Score score : inquireScore) {
+            if(round == score.getScoreRound()) {
+                resultScore = score;
+                countRound++;
+            }
+        }
+        if (countRound != 1) {
+            System.out.println("입력된 시험 회차와 일치하는 과목의 시험 회차가 없습니다.");
+            return;
+        }
 
+        System.out.println("\n회차별 등급을 조회합니다...");
+
+        System.out.println("\n수강생 : "+studentId+" / 수강 과목 : "+subjectName);
+        System.out.println("시험 회차 : "+round+" / 시험 등급 : "+resultScore.getScoreRank());
 
         System.out.println("\n등급 조회 성공!");
     }
