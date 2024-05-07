738d88654800e97d2eae9fb4ff92d6bacf843ac6
JKhan0408 <workhjk804@gmail.com>
2024-05-07 10:57:27 +0900

    수강생 등록 - 등록 확인 코드 삭제

--- src/camp/function/StudentDAO.java
+++ src/camp/function/StudentDAO.java
@@ -10,9 +10,14 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import camp.model.Student;
+
+
+
 public class StudentDAO {
     private static Scanner sc = new Scanner(System.in);
     SubjectDAO subjectDAO = new SubjectDAO();
+    InitializeData initializeData = new InitializeData();
 
     // 고유 번호
     private int studentIndex;
@@ -179,14 +184,39 @@ public class StudentDAO {
         studentStore.add(student);
 
         System.out.println("\n수강생 등록 성공!\n");
-        System.out.println(studentStore);
+//        System.out.println(studentStore);
     }
+
 
     // 수강생 목록 조회
     public void inquireStudent() {
+        // 학생 목록 가져오기
+        List<Student> studentsList = getStudentStore();
+
         System.out.println("\n수강생 목록을 조회합니다...");
-        // 기능 구현
-        System.out.println("\n수강생 목록 조회 성공!");
+        System.out.print("조회할 학생의 이름을 입력하세요 : ");
+        String name = sc.next();
+        System.out.print("조회할 학생의 고유번호을 입력하세요 : ");
+        String index = sc.next();
+
+        // 이름과 고유번호 확인
+        Optional<Student> optionalStudentName = studentsList.stream().filter(s-> s.getStudentName().equals(name)).findFirst();
+        Optional<Student> optionalStudentIndex = studentsList.stream().filter(s -> s.getStudentId().equals(index)).findFirst();
+
+
+        // 조회하기
+        if(optionalStudentIndex.isPresent() && optionalStudentName.isPresent()){
+            Student student = optionalStudentIndex.get();
+            System.out.println("=== 조회내용 ===");
+            System.out.println("이름: "+student.getStudentName());
+            System.out.println("고유번호 : "+student.getStudentId());
+            System.out.println("상태 : "+student.getStudentStatus());
+            System.out.println("과목 : "+student.getStudentSubjects());
+            System.out.println("\n수강생 목록 조회 성공!");
+        }else{
+            System.out.println("다시한번 확인해주세요.");
+        }
+
     }
 
     // 고유 번호 증가
@@ -195,6 +225,3 @@ public class StudentDAO {
         return INDEX_TYPE_STUDENT + studentIndex;
     }
 }
-
-
-
